ffdc47be574f59a83058d12dfcad3c41c482e1cf
MarkHash <makoto.hashimoto06@gmail.com>
2023-02-22 17:16:50 +1100

    completed training, scoring, deployment

--- deployment.py
+++ deployment.py
@@ -7,6 +7,7 @@ from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
+import subprocess
 
 
 
@@ -14,14 +15,23 @@ import json
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
-
+output_folder_path = os.path.join(os.getcwd(), config['output_folder_path']) 
+prod_deployment_path = os.path.join(os.getcwd(), config['prod_deployment_path'])
+model_path = os.path.join(os.getcwd(), config['output_model_path']) 
+model_file_name = 'trainedmodel.pkl'
+score_file_name = 'latestscore.txt'
+step_record_name = 'ingestedfiles.txt'
 
 ####################function for deployment
-def store_model_into_pickle(model):
+def store_model_into_pickle():
     #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
+    
+    res1 = subprocess.run(['cp', os.path.join(model_path, model_file_name), os.path.join(prod_deployment_path, model_file_name)], capture_output=True)
+    res2 = subprocess.run(['cp', os.path.join(model_path, score_file_name), os.path.join(prod_deployment_path, score_file_name)], capture_output=True)
+    res3 = subprocess.run(['cp', os.path.join(output_folder_path, step_record_name), os.path.join(prod_deployment_path, step_record_name)], capture_output=True)
+    # print(res1)
+    # print(res2)
+    # print(res3)    
 
+if __name__ == '__main__':
+    store_model_into_pickle()
--- ingestion.py
+++ ingestion.py
@@ -11,8 +11,8 @@ from datetime import datetime
 with open('config.json','r') as f:
     config = json.load(f) 
 
-input_folder_path = os.getcwd() + config['input_folder_path']
-output_folder_path = os.getcwd() + config['output_folder_path']
+input_folder_path = os.path.join(os.getcwd(), config['input_folder_path'])
+output_folder_path = os.path.join(os.getcwd(), config['output_folder_path'])
 
 output_file_name = 'finaldata.csv'
 step_record_name = 'ingestedfiles.txt'
@@ -30,21 +30,21 @@ def merge_multiple_dataframe():
         #check if the file extension is '.csv'
         if os.path.splitext(file)[1].lower() == '.csv':
             #read CSV file into dataframe and merge into one
-            df = pd.read_csv(input_folder_path + '/' + file)
+            df = pd.read_csv(os.path.join(input_folder_path, file))
             merged_df = merged_df.append(df).reset_index(drop=True)
 
             #record the time and file of ingestion
             dateTimeObj = datetime.now()
             time_now = str(dateTimeObj.year) + '/' + str(dateTimeObj.month) + '/' + str(dateTimeObj.day)
             all_records = [input_folder_path, file, len(df.index), time_now]
-            step_record_file = open(output_folder_path + '/' + step_record_name, 'a')
+            step_record_file = open(os.path.join(output_folder_path, step_record_name), 'a')
             for element in all_records:
                 step_record_file.write(str(element) + ' ')
             step_record_file.write('\n')
 
     #remove duplicate record and export to csv file
     merged_df = merged_df.drop_duplicates()
-    merged_df.to_csv(output_folder_path + '/' + output_file_name)
+    merged_df.to_csv(os.path.join(output_folder_path, output_file_name))
 
 if __name__ == '__main__':
     merge_multiple_dataframe()
